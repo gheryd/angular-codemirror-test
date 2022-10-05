a349a5098f6eaa76a93924368f0134b58822fdea
gerardo.damone <gerardo.damone@rgigroup.com>
2022-10-05 12:10:45 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+
+
+import 'codemirror/lib/codemirror';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/addon/mode/simple';
 
 import { AppComponent } from './app.component';
+import { EditorComponent } from './editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    CodemirrorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/editor.component.ts
@@ -0,0 +1,61 @@
+import { AfterViewInit, Component, OnInit, ViewChild, ViewEncapsulation } from "@angular/core";
+import { CodemirrorComponent } from "@ctrl/ngx-codemirror";
+
+declare var CodeMirror: any;
+
+@Component({
+    selector: 'editor',
+    template: `
+        <ngx-codemirror #codeMirror
+            [(ngModel)]="code"
+            [options] = "options"
+        ></ngx-codemirror>
+        <pre>{{code|json}}</pre>
+    `,
+    styleUrls: ['./editor.component.scss'],
+    encapsulation: ViewEncapsulation.None
+
+})
+export class EditorComponent implements OnInit, AfterViewInit {
+    
+    
+    options = {
+        lineNumbers: true,
+        // theme: 'material',
+        mode: 'javascript'
+    }
+    code: string = ''
+    
+    @ViewChild(CodemirrorComponent, {static: false}) private cmComponent!: CodemirrorComponent;
+
+    ngOnInit(): void {
+    }
+
+
+    ngAfterViewInit(): void {
+        setTimeout(
+            () => {
+                const codeMirror = this.cmComponent.codeMirrorGlobal.__zone_symbol__value.default;
+                codeMirror.defineSimpleMode('ppnext', {
+                    start: [
+                        {regex: /(?:IF|ELSE|ENDIF|WHILE|ENDWHILE|FOR|TO|NEXT)\b/, token: "keyword"},
+                        {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: "number"},
+                        {regex: /[-+\/*=<>!]+/, token: "operator"}
+                    ]
+                });
+                this.options = {
+                    lineNumbers: true,
+                    // theme: 'material',
+                    mode: 'ppnext'
+                }
+                // const d = this.cmComponent.codeMirrorGlobal.__zone_symbol__value.default;
+                // console.log('defineSimpleMode------>', d.defineSimpleMode);
+                // console.log('------>', this.cmComponent.codeMirror);
+                // var codeMirror = typeof CodeMirror !== 'undefined' ? CodeMirror : import('codemirror');
+                // console.log('CodeMirror: ', codeMirror);
+            }
+        );
+        
+    }
+    
+}
