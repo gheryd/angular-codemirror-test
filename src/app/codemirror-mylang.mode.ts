12676d20b0c0d7a1b6a9b9285f5f59fe08924c15
gerardo.damone <gerardo.damone@rgigroup.com>
2022-10-19 09:23:23 +0200

    impl other token in simple mode

--- src/app/codemirror-mylang.mode.ts
+++ src/app/codemirror-mylang.mode.ts
@@ -1,16 +1,20 @@
 import * as CodeMirror from "codemirror";
 
+
 export function initCodeMirrorMyLang(){
 
-    CodeMirror.defineSimpleMode('mylang', {
-        start: [
-            {regex: /(?:IF|ELSE|ENDIF|WHILE|ENDWHILE|FOR|TO|NEXT|PROVAA)\b/, token: "keyword"},
-            {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: "number"},
-            {regex: /[-+\/*=<>!]+/, token: "operator"}
-        ]
-    });
+    const start = [
+        {regex: /;.*/, token: "comment"},
+        {regex: /'(?:[^\\]|\\.)*?(?:'|$)/, token: "string"},
+        {regex: /(?:IF|ELSE|ENDIF|WHILE|ENDWHILE|FOR|TO|NEXT)\b/, token: "keyword", sol:true},
+        {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i, token: "number"},
+        {regex: /[-+\/*=<>!]+/, token: "operator"},
+        
+    ]
+    
+    const states = { start };
 
-
+    CodeMirror.defineSimpleMode( 'mylang', states );
 
 }
 
